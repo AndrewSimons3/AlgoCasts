48032ff0b68c6aad7b9c2517cde9321f3424b85b
AndrewSimons3 <simdrew07@gmail.com>
2021-06-13 11:48:15 -0500

    updated solution 1 in anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -12,33 +12,37 @@
 
 
 // SOLUTION 1
-// function anagrams(stringA, stringB) {
-//   const aCharMap = buildCharMap(stringA);
-//   const bCharMap = buildCharMap(stringB);
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
 
-//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-//     return false;
-//   }
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
+  }
 
-//   for (let char in aCharMap) {
-//     if (aCharMap[char] !== bCharMap[char]) {
-//       return false;
-//     }
-//   }
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
 
-//   return true;
-// }
+  return true;
+}
 
-// function buildCharMap (str) {
-//   const charMap = {};
 
-//   for (let char of str.replace(/[^\W]/g, "").toLowerCase()) {
-//     charMap[char] = charMap[char] + 1 || 1;
-//   }
+function buildCharMap (str) {
+  const charMap = {};
+
+  for (let char of str.replace(/[^\W]/g, "").toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
   
-//   return charMap;
-// }
+  return charMap;
+}
 
+
+
+//SOLUTION 2
 function anagrams(stringA, stringB) {
   return cleanString(stringA) === cleanString(stringB)
 }
